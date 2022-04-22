ad8aa3540e67c162f3d1463fa45bf3a75dafb117
0bealan0 <beatriz.valenzuela.ajenjo@gmail.com>
2022-04-22 08:40:10 +0200

    bd

--- cervezasController.js
+++ cervezasController.js
@@ -1,5 +1,8 @@
 const cervezasController = {};
 
 cervezasController.list = (req, res) => res.send("Lista de cervezas");
-
+cervezasController.create = (req, res) =>
+  res.send(
+    `Cerveza creada ${req.body.nombre} Características: ${req.body.descripción}`
+  );
 module.exports = cervezasController;
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+//DEPENDENCIAS
+
 //importa, el resultado de express se guarda en app, es la instancia de nuestro servidor
 const express = require("express");
 const cervezasRouter = require("./cervezasRouter.js");
+const Cerveza = require("./cervezasModel");
 const app = express();
-
 const path = require("path");
-
+require("./db");
 const staticRoute = path.join(__dirname, "public");
+const bodyParser = require("body-parser");
+
+//MIDDLEWARES
 //cuando reciba una petición le responderemos:
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
@@ -14,7 +23,10 @@ app.get("/", (req, res) => {
 
 app.use("/static", express.static(staticRoute));
 //si alguien pide cervezas le voy a enroutar yo la peticion, es una especie de middelware intermedio que genera otra ruta.
-app.use("/cervezas", cervezasRouter);
+app.use("/api/cervezas", cervezasRouter);
+
+//RUTAS Y ENROUTADORES
+
 app.use((req, res, next) => {
   const now = new Date().toString();
   console.log(`${now} ${req.method} ${req.url}`);
@@ -27,4 +39,7 @@ app.get("/bancos", function (req, res) {
 app.get("/contactar", function (req, res) {
   res.send("Página para contactar");
 });
-app.listen(3000);
+//esta funcion permite escribir el mensaje cuando se escuche por el puerto 3000
+app.listen(3000, function () {
+  console.log(`Escuchando por el puerto 3000`);
+});
